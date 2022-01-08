8acf2d2d1da1ade086bcf30fef58ac528055721c
DougFunny <douglaseduardo220483@gmail.com>
2022-01-08 16:36:59 -0300

    Exercício 1 Python em 3
    Primeiro Olá Mundo!

--- ex001.py
+++ ex001.py
@@ -4,8 +4,8 @@ print('''\033[32m##########################
 print('-='*30)
 print('Hello World!!!')#English
 print('¡¡¡Hola mundo!!!')#Spanish
-print('你好世界！')#Chinise
+print('你好世界！')#Chinese
 print('Olá, Mundo!!!')#Portuguese
-print('こんにちは！')#Japonese
-print('Tô fora da maldição!!!!')
+print('こんにちは！')#Japanese
+print('Não vou cai na maldição!!!!')
 print('-='*30)
